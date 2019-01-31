ecaa1d97c7ca87644c57204e480c6e339a28b013
Khofidin <offiedz@gmail.com>
2019-01-31 09:24:07 +0700

    Dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import posts from '../data/posts';
+import Link from 'next/link'
 
 export default () => (
   <div>
-    <Nav/>
-    <hr/>
-    Index
+    <Nav />
+    <hr />
+    <ul>
+      {posts.map((post, index) => (
+        <li key={index}>
+          <Link href={{pathname: '/second', query: {id: index}}}>
+            <a>{post.title}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
--- pages/second.js
+++ pages/second.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Nav from '../components/Nav'
+import posts from '../data/posts'
+import Error from 'next/error'
 
-export default () => (
-  <div>
-    <Nav/>
-    <hr/>
-    Second
-  </div>
+export default ({ url: { query: { id } } }) => (
+  (posts[id]) ? (
+    <div>
+      <Nav />
+      <hr />
+      <h1>Second {posts[id].title} page</h1>
+    </div>
+  ) : (
+      <Error statusCode={404} />
+    )
 )
