ed043d967c24b7cdd6e92ee59e378d1d60510150
Khofidin <offiedz@gmail.com>
2019-01-31 10:41:44 +0700

    Add graph

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Nav from '../components/Nav';
 import posts from '../data/posts';
 import Link from 'next/link'
+import 'react-vis/dist/style.css'
+import {HorizontalGridLines, LineSeries, XAxis, XYPlot, YAxis} from 'react-vis'
 
 export default () => (
   <div>
@@ -16,5 +18,20 @@ export default () => (
         </li>
       ))}
     </ul>
+    <XYPlot
+      width={300}
+      height={300}>
+      <HorizontalGridLines/>
+      <LineSeries
+        data={[
+          {x: 1, y: 10},
+          {x: 2, y: 3},
+          {x: 3, y: 5},
+          {x: 4, y: 17},
+          {x: 5, y: 21},
+        ]}/>
+        <XAxis/>
+        <YAxis/>
+    </XYPlot>
   </div>
 );
